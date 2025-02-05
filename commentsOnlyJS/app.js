b9912cde74146cdbf4ec90451c60871824ee7b1b
Abdelrahman Ayman <55602893+AbdelrahmanAyman48@users.noreply.github.com>
2025-02-05 17:12:36 +0200

    Add files via upload

---
+++ commentsOnlyJS/app.js
@@ -0,0 +1,14 @@
+// Personal API Key for OpenWeatherMap API
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = 'fb0bb0146bd5dbe22fbef6296250c239';
+// Event listener to add function to existing HTML DOM element
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
+
